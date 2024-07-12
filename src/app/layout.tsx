db4d53e9ce7108fdbc832be7c1ac12a1a12005bb
vijay <vijaykv2228@gmail.com>
2024-07-12 21:50:05 +0530

    (feat) : add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { Roboto } from 'next/font/google';
 import Head from 'next/head';
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
@@ -13,6 +14,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EQMY1YEC95"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LL85JFDCVV');
+          `}
+        </Script>
+      </head>
       <Head>
         <title>Rustcrab - The Non-Crap Repo for Rust Developers</title>
         <meta name="description" content="The best repository for Rust developers" />
