7035405c8910f18d02867071b7eb351e527f70a0
Pavan Kumar <pavan@change.local>
2024-07-13 17:29:59 +0530

    refactor: Update social media links in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,40 @@
 "use client";
 
+import Link from 'next/link';
 import { FaLinkedin, FaYoutube, FaDiscord, FaInstagram } from 'react-icons/fa';
 import { FaXTwitter, } from 'react-icons/fa6';
+
+const data = [
+  {
+    href: 'https://x.com/FrancescoCiull4',
+    icon: FaXTwitter,
+  },
+  {
+    href: 'https://www.linkedin.com/in/francesco-ciulla-roma/',
+    icon: FaLinkedin,
+  },
+  {
+    href: 'https://www.youtube.com/@francescociulla',
+    icon: FaYoutube,
+  },
+  {
+    href: 'https://discord.gg/4c-784142072763383858',
+    icon: FaDiscord,
+  },
+  {
+    href: 'https://www.instagram.com/francescociull4/',
+    icon: FaInstagram,
+  },
+];
 
 export default function Footer() {
   return (
     <footer className="flex justify-center space-x-6 p-4">
-      <a href="https://x.com/FrancescoCiull4" target="_blank" rel="noopener noreferrer" className="text-2xl text-rust-orange">
-        <FaXTwitter />
-      </a>
-      <a href="https://www.linkedin.com/in/francesco-ciulla-roma/" target="_blank" rel="noopener noreferrer" className="text-2xl text-rust-orange">
-        <FaLinkedin />
-      </a>
-      <a href="https://www.youtube.com/@francescociulla" target="_blank" rel="noopener noreferrer" className="text-2xl text-rust-orange">
-        <FaYoutube />
-      </a>
-      <a href="https://discord.gg/4c-784142072763383858" target="_blank" rel="noopener noreferrer" className="text-2xl text-rust-orange">
-        <FaDiscord />
-      </a>
-      <a href="https://www.instagram.com/francescociull4/" target="_blank" rel="noopener noreferrer" className="text-2xl text-rust-orange">
-        <FaInstagram />
-      </a>
+      {data.map((data,index) => (
+        <Link href={data.href} key={index} target="_blank" rel="noopener noreferrer" className="text-2xl text-rust-orange">
+        {<data.icon/>}
+      </Link>
+))}
     </footer>
   );
 }
