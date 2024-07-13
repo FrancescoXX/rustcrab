2445711c73fbf143fcd9bed4f8272454cfc6b493
FrancescoXX <francescociullaroma@gmail.com>
2024-07-13 17:09:16 +0200

    changed main page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Image from "next/image";
-import GitHubStars from "../components/GitHubStars";
-import SubstackEmbed from "../components/SubstackEmbed";
-import DevToolsSection from "@/components/DevToolsSection";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Image from 'next/image';
+import GitHubStars from '../components/GitHubStars';
+import SubstackEmbed from '../components/SubstackEmbed';
+import DevToolsSection from '@/components/DevToolsSection';
 
 export default function Home() {
   return (
@@ -11,28 +11,18 @@ export default function Home() {
       <Header />
       <div className="border-t-4 border-rust-orange"></div> {/* Orange line */}
       <main className="flex-grow flex flex-col items-center justify-center text-center space-y-4">
+        <br />
         <h1 className="text-4xl font-bold flex items-center space-x-2">
-          <a
-            href="https://github.com/FrancescoXX/rustcrab"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://github.com/FrancescoXX/rustcrab" target="_blank" rel="noopener noreferrer">
             Rustcrab
           </a>
-          <Image
-            src="/rust_lgo_720.png"
-            alt="Rust Logo"
-            width={50}
-            height={50}
-          />
+          <Image src="/rust_lgo_720.png" alt="Rust Logo" width={50} height={50} />
         </h1>
-        <p className="text-xl">The non-crap repo for Rust Developers.</p>
+        <p className="text-xl">The non-crap resource for Rust Developers.</p>
         <GitHubStars repo="FrancescoXX/rustcrab" />
         <br />
         <br />
-        <p className="text-xl">
-          🚧 An Open source project to have everything you need to learn Rust 🚧
-        </p>
+        <p className="text-xl">🦀 An Open source project with everything you need to learn about Rust 🦀</p>
         <br />
         <p className="text-xl">Subscribe to get the latest updates</p>
         <SubstackEmbed /> {/* Embedding Substack iframe */}
