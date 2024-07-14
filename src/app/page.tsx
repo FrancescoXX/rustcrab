0cff11132ba08d4b492071b190544d2951f610df
FrancescoXX <francescociullaroma@gmail.com>
2024-07-14 04:15:32 +0200

    fixing project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,70 @@
+"use client";
+
+import { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Image from 'next/image';
 import GitHubStars from '../components/GitHubStars';
 import SubstackEmbed from '../components/SubstackEmbed';
-import DevToolsSection from '@/components/DevToolsSection';
-import CookieConsentBanner from '@/components/CookieConsent';
-import BooksSection from '@/components/BooksSection';
-import ProjectsSection from "@/components/ProjectsSection";
+import CookieConsentBanner from '../components/CookieConsent';
+import Books from '../components/BooksSection';
+import DevToolsSection from '../components/DevToolsSection';
+import ProjectsSection from '../components/ProjectsSection';
+import Navbar from '../components/navbar/Navbar';
+import MobileNav from '../components/navbar/MobileNav';
 
 
 export default function Home() {
+  const [currentSection, setCurrentSection] = useState('home');
+
+  const renderSection = () => {
+    switch (currentSection) {
+      case 'books':
+        return <Books />;
+      case 'dev tools':
+        return <DevToolsSection />;
+      case 'projects':
+        return <ProjectsSection />;
+      default:
+        return (
+          <div className="flex flex-col items-center justify-center text-center space-y-4">
+            <br />
+            <h1 className="text-4xl font-bold flex items-center space-x-2">
+              <a
+                href="https://github.com/FrancescoXX/rustcrab"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Rustcrab
+              </a>
+              <Image
+                src="/rust_lgo_720.png"
+                alt="Rust Logo"
+                width={50}
+                height={50}
+              />
+            </h1>
+            <p className="text-xl">The non-crap resource for Rust Developers.</p>
+            <GitHubStars repo="FrancescoXX/rustcrab" />
+            <br />
+            <br />
+            <p className="text-xl">
+              🦀 An Open source project with everything you need to learn about Rust 🦀
+            </p>
+            <br />
+            <p className="text-xl">Subscribe to get the latest updates</p>
+            <SubstackEmbed />
+          </div>
+        );
+    }
+  };
+
   return (
     <div className="bg-white dark:bg-black min-h-screen text-black dark:text-white flex flex-col">
       <Header />
+      <Navbar setSection={setCurrentSection} />
       <main className="flex-grow flex flex-col items-center justify-center text-center space-y-4">
-        <br />
-        <h1 className="text-4xl font-bold flex items-center space-x-2">
-          <a
-            href="https://github.com/FrancescoXX/rustcrab"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Rustcrab
-          </a>
-          <Image
-            src="/rust_lgo_720.png"
-            alt="Rust Logo"
-            width={50}
-            height={50}
-          />
-        </h1>
-        <p className="text-xl">The non-crap resource for Rust Developers.</p>
-        <GitHubStars repo="FrancescoXX/rustcrab" />
-        <br />
-        <br />
-        <p className="text-xl">
-          🦀 An Open source project with everything you need to learn about Rust
-          🦀
-        </p>
-        <br />
-        <p className="text-xl">Subscribe to get the latest updates</p>
-        <SubstackEmbed /> {/* Embedding Substack iframe */}
-        <DevToolsSection />
-        <BooksSection/>
-        <ProjectsSection />
+        {renderSection()}
       </main>
       <CookieConsentBanner />
       <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,59 +1,62 @@
 'use client';
 import React from 'react';
-import Link from 'next/link';
 
 interface menuObject {
-	items: string;
-	link: string;
+  items: string;
+  link: string;
+}
+
+interface NavbarProps {
+  setSection: (section: string) => void;
 }
 
 export const menuItems: menuObject[] = [
-	{
-		items: 'Courses',
-		link: '/courses',
-	},
-	{
-		items: 'books',
-		link: '/books',
-	},
-	{
-		items: 'resources',
-		link: '/resources',
-	},
-	{
-		items: 'projects',
-		link: '/projects',
-	},
-	{
-		items: 'dev tools',
-		link: '/dev',
-	},
-	{
-		items: 'people',
-		link: '/people',
-	},
-	{
-		items: 'events',
-		link: '/events',
-	},
+  {
+    items: 'Courses',
+    link: 'courses',
+  },
+  {
+    items: 'books',
+    link: 'books',
+  },
+  {
+    items: 'resources',
+    link: 'resources',
+  },
+  {
+    items: 'projects',
+    link: 'projects',
+  },
+  {
+    items: 'dev tools',
+    link: 'dev tools',
+  },
+  {
+    items: 'people',
+    link: 'people',
+  },
+  {
+    items: 'events',
+    link: 'events',
+  },
 ];
 
-const Navbar = () => {
-	return (
-		<div className=' hidden sm:block '>
-			<ul className='flex space-x-4 capitalize '>
-				{menuItems.map((items, index) => {
-					return (
-						<li className='cursor-pointer transition ease-in-out' key={index}>
-							<Link href={items.link} className='hover:text-orange-500'>
-								{items.items}
-							</Link>
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
+const Navbar: React.FC<NavbarProps> = ({ setSection }) => {
+  return (
+    <div className="hidden sm:block">
+      <ul className="flex space-x-4 capitalize">
+        {menuItems.map((item, index) => (
+          <li
+            key={index}
+            className="cursor-pointer transition ease-in-out"
+            onClick={() => setSection(item.link)}
+          >
+            <span className="hover:text-orange-500">{item.items}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
