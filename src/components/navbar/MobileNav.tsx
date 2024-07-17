3e5fc49025ab50757b90f7ae109c0744e7ac19d8
Harsh Thakur <harshthakur0025@gmail.com>
2024-07-17 15:13:16 +0000

    Fixed the Routing in Nav

--- src/components/navbar/MobileNav.tsx
+++ src/components/navbar/MobileNav.tsx
@@ -13,9 +13,9 @@ export const menuItems: menuObject[] = [
   { items: "Books", link: "#books" },
   { items: "Projects", link: "#projects" },
   { items: "Dev Tools", link: "#dev_tools" },
-  { items: "Lesson", link: "#lesson" },
+  { items: "Lesson", link: "#lessons" },
   { items: "People", link: "#people" },
-  { items: "DSA", link: "#dsa" },
+  { items: "DSA", link: "#dsas" },
 ];
 
 const MobileNav: React.FC = () => {
