5f30d2554b0a674a8d55650bdc5b8903eec0a2be
Tommaso Allevi <tomallevi@gmail.com>
2024-07-26 17:00:09 +0200

    Some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // "dark" "light" or null. Null means no preference.
   const initDarkModeDetection = `
 (function () {
 const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -23,12 +22,16 @@ if ((!themeChosen && isDarkModePreferred) || themeChosen === "dark") {
   document.documentElement.classList.add("dark");
   localStorage.theme = 'dark';
 }
-})()`
+})()`;
 
   return (
     <html lang="en">
       <head>
-      <script type="application/javascript" id="dark-mode-detection" dangerouslySetInnerHTML={{ __html: initDarkModeDetection }}></script>
+      <script
+          type="application/javascript"
+          id="dark-mode-detection"
+          dangerouslySetInnerHTML={{ __html: initDarkModeDetection }}
+        ></script>        
       <meta property="og:title" content="Rustcrab" />
         <meta
           property="og:description"
